be677d439391307856949f8784ff232b0dc59236
Rhett van Korlaar <rhettvankorlaar@gmail.com>
2021-04-26 00:54:49 +0200

    Removes unused files

---
+++ src/main/java/nl/themagrid/helper/config/JHipsterInfo.java
@@ -0,0 +1,10 @@
+package nl.themagrid.helper.config;
+
+import io.quarkus.arc.config.ConfigProperties;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@ConfigProperties(prefix = "jhipster.info")
+public interface JHipsterInfo {
+    @ConfigProperty(name = "swagger.enable", defaultValue = "true")
+    Boolean isEnable();
+}
---
+++ src/main/java/nl/themagrid/helper/config/JHipsterProperties.java
@@ -0,0 +1,12 @@
+package nl.themagrid.helper.config;
+
+import io.quarkus.arc.config.ConfigProperties;
+
+@ConfigProperties(prefix = "jhipster")
+public class JHipsterProperties {
+    public Oidc oidc;
+
+    public static class Oidc {
+        public String logoutUrl;
+    }
+}
